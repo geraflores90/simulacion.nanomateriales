84bc48547764305ce44a6c4292da5cc12b67e7d4
geraflores90 <geraflores90@gmail.com>
2017-02-06 02:00:34 -0600

    Add files via upload

--- Tarea_02_movimiento.m
+++ Tarea_02_movimiento.m
@@ -5,8 +5,8 @@ clc
 l=2; % Distancia de eje
 p=1; % Resolución (Determinar en quÃé parte del código se introducirá ésta 
      % variable)
-n=2; % Número de partículas
-d=2; % Dimensión
+n=3; % Número de partículas
+d=3; % Dimensión
 nm=3; % Número de movimientos que realizará la partícula.
 
 k=(2*l/p)+1; % Posiciones permitidas
@@ -16,31 +16,37 @@ m=round(unifrnd(1,k,n,d)) % Matriz aleatoria de n partículas con d dimensiones
 
 for i=1:n
   for j=1:d
-  dist1(i,j)=(m(i,j)-1)*p+(-l); %Asigna la posición en función de p
+  dist1(i,j)=(m(i,j)-1)*p+(-l) %Asigna la posición en función de p
   end                              
 end
 
 x=randi([-1,1],n,d)
 
 for i=1:n
-    
-    
-    if x(i,1)==0
-         x(i,2)=round(randi([0,1],1,1))
-            if x(i,2)==0
-                x(i,2)=-1
+    for j=1:d
+    z=j+1;
+  
+    if x(i,j)==0
+   
+            while z<=d 
+            x(i,z)=round(randi([0,1],1,1))
+                if x(i,z)==0
+                    
+                end
             end
+       
          
-    elseif x(i,1)~=0
-        x(i,2)=0
+    elseif x(i,j)~=0
+        x(i,z)=0
     end 
+    end
     
 end
 
 %Se logra generar una matriz de unos y ceros para dos dimensiones
 %falta generalizar para n dimensiones.
 
-%bibubipi
+
 
 % for i=1:nm
 % for j=1:d
