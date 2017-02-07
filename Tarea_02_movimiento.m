6f19f1eab4a3f99daa59500e9020da29ee5ec902
geraflores90 <geraflores90@gmail.com>
2017-02-06 23:26:51 -0600

    Add files via upload

--- Tarea_02_movimiento.m
+++ Tarea_02_movimiento.m
@@ -1,42 +1,42 @@
-% Se intetará introducir movimiento a la partícula mediante la introducción de 
+% Se intetara introducir movimiento a la particula mediante la introduccion de 
 % la variable de t (tiempo)
 clear
 clc
 l=2; % Distancia de eje
-p=1; % Resolución (Determinar en qué parte del código se introducirá ésta 
+p=1; % Resolucion (Determinar en que parte del codigo se introducira esta 
      % variable)
-n=3; % Número de partículas
-d=3; % Dimensión
-nm=3; % Número de movimientos que realizará la partícula.
+n=3; % Numero de particulas
+d=3; % Dimension
+nm=3; % Numero de movimientos que realizara la particula.
 
 k=(2*l/p)+1; % Posiciones permitidas
 
-m=round(unifrnd(1,k,n,d)); % Matriz aleatoria de n partículas con d dimensiones 
+m=round(unifrnd(1,k,n,d)); % Matriz aleatoria de n particulas con d dimensiones 
                           % redondeada
 
 for i=1:n
   for j=1:d
-  dist(i,j)=(m(i,j)-1)*p+(-l); %Asigna la posición en función de p
+  dist(i,j)=(m(i,j)-1)*p+(-l); % Asigna la posicion en funcion de p
   end                              
 end
 
 z=[];
 for i=1:n
     x=zeros(1,d);
-    b=randi([1,d],1,1);
-    x(1,b)=randi([0,1]);
-    if x(1,b)>0
+    b=randi([1,d],1,1); % Selecciona aleatoriamente una dimension a modificar.
+    x(1,b)=randi([0,1]); % Subprograma que aleatoriamete elige valor positivo o 
+    if x(1,b)>0          % negativo a modificar.
         x(1,b)=1;
     else
         x(1,b)=-1;
     end 
-z=[z;x]; %Matriz aleatoria de unos y ceros.
+z=[z;x]; % Matriz aleatoria de unos y ceros.
 end
 
 dist
-mov=dist+z
+mov=dist+z;
 
-for i=1:n
+for i=1:n % Rebotar la particula que se encuentra en el limite permitido.
     for j=1:d
         comp=l-abs(mov(i,j));
         if mov(i,j)<0
