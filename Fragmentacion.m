365ee94e0973bb850caa7e65356dc84feb17af16
geraflores90 <geraflores90@gmail.com>
2017-03-03 15:31:03 -0600

    Add files via upload
    Funciona, lol

--- Fragmentacion.m
+++ Fragmentacion.m
@@ -1,116 +1,80 @@
-% Fragmentación. Simular una interaccion cuando dos o mas particulas de la 
+% Fragmentacion. Simular una interaccion cuando dos o mas particulas de la 
 % matriz ocupan la misma posicion.
 % Se introduce la variable tam (tamaño de particula).
 
 clear
 clc
-l=3; % Distancia de eje.
-p=1; % Resolucion. 
-n=10; % Numero de particulas.
-d=2; % Dimension.
-nm=10; % Numero de movimientos que realizara la particula.
-tam=2; %Tamaño inicial de todas las particulas.
+l = 4;
+umbral = 0.1;
+n = 30; % Numero de particulas.
+d = 2; % Numero de dimensiones.
+t = 30; % Numero de pasos.
+vel = 0.5; % Valor maximo de paso dimensional permitido.
+pos = unifrnd(0, l, n, d);
+tam = 20 * ones(1, n); % Vector de tamaño para n particulas.
+nmax = 100; % Maximo numero de particulas que pueden existir.
 
-k=(2*l/p)+1; % Posiciones permitidas
-
-m=round(unifrnd(1,k,n,d)); % Matriz aleatoria de n particulas con d dimensiones 
-                          % redondeada
-                          
-vector_tam=[]; % Vector de tamaño para n particulas.
-for i=1:n
-  vector_tam=[vector_tam;tam];
-end
-
-for i=1:n
-  for j=1:d
-  dist(i,j)=(m(i,j)-1)*p+(-l); % Asigna la posicion en funcion de p
-  end                              
-end
-
-posicion_original=dist
-
-for w=1:nm % Repite el numero de movimientos deseado.
-
-  z=[];
-    
-  for i=1:n
-      x=zeros(1,d);
-      b=randi([1,d],1,1); % Selecciona aleatoriamente una dimension a modificar.
-      x(1,b)=randi([0,1]); % Subprograma que aleatoriamete elige valor positivo o 
-      if x(1,b)>0          % negativo a modificar.
-          x(1,b)=1;
-      else
-          x(1,b)=-1;
-      end 
-  z=[z;x]; % Matriz aleatoria de unos y ceros.
-  end
-
-  dist;
-  mov=dist+z;
-
-  for i=1:n % Rebotar la particula que se encuentra en el limite permitido.
-      for j=1:d 
-          comp=l-abs(mov(i,j));
-          if mov(i,j)<0
-              nop=-1;
-          else
-              nop=1;
-          end
-          if comp<0
-              comp=comp+l;
-              mov(i,j)=nop*comp;
-          end
-      end
-  end
-    
-  plot(mov(:,1),mov(:,2),'o','markersize',14); % Intentar generalizar a n dim
-  axis([-l l -l l])
-  
-  vh = get(gca); % No se que hace xdxdxdxd
-  pause(.1);
+for paso = 1:t 
+  pos += unifrnd(-vel, vel, n, d);
+  for i = 1 : n 
+    for j = 1 : d 
+      if pos(i, j) < 0
+        pos(i, j) = abs(pos(i, j));
+      elseif pos(i, j) > l
+        pos(i, j) = 2 * l - pos(i, j);
+      endif
+    endfor
+  endfor    
+  plot(pos(:, 1), pos(:, 2), 'o', 'markersize', 6); 
+  axis([0 l 0 l]);
+  pause(0.2);
+  choque = [];
   contador = 0;
-  
-  choque=[];
-  
-  if n>1
-    for i=1:n-1
-      for j=(i+1):n
-        %if (sum(mov(i,:)==mov(j,:)) == d)
-        if mov(i,:)==mov(j,:)
-          contador=contador+1;
-          crash=mov(i,:); % Choques individuales en la matriz.
-          choque=[choque;crash] % Adjunta los choques individuales en una sola matriz.
-          hold on
-          plot(choque(:,1),choque(:,2),'x','markersize',14)
-          axis([-l l -l l])
-          plot([0],[0],'ro')
-          hold off
-          %cat=mov(i,:);
-          %dog=mov(j,:);
-          moneda=randi([0,1]);
-          if moneda>0
-            mov(i,:)=[]
-            n=n-1;
-          else
-            mov(j,:)=[]
-            n=n-1;
-          end  
-        end
-      end
-    end
-  end
-  
-  saveas(gcf,strcat('figura',num2str(w),'.jpg'))
-  
-  mov
-  contador
-  dist=mov;
-
-end
-
-
-
-
-
-
-  
+  sig = n + 1;
+  for i = 1 : n-1
+    for j = (i + 1) : n
+      de = sqrt((pos(i, :) - pos(j, :)).^2);
+      if de < umbral
+        dif = umbral - de;
+        if rand() < dif / umbral
+          ti = tam(i);
+          if ti > 2
+            tam(i) = 1 + randi(ti - 2);
+            fi = ti - tam(i);
+            tam(sig) = fi; 
+            pos(sig, :) = pos(i, :);
+            sig++;
+          endif
+          tj = tam(j);
+          if tj > 2
+            tam(j) = 1 + randi(tj - 2);
+            fj = tj - tam(j);
+            tam(sig) = fj; 
+            pos(sig, :) = pos(j, :);
+            sig++;
+          endif
+          contador++;
+          choque(contador, :) = pos(i,:); 
+        endif 
+      endif  
+    endfor
+  endfor
+  if contador > 0
+    hold on;
+    plot(choque(:, 1), choque(:, 2), 'x', 'markersize', 12)
+    axis([0 l 0 l]);
+    pause(0.1);
+    hold off;
+  endif
+  n = sig - 1
+  avg = mean(tam)
+  total = sum(tam)
+  menor = min(tam)
+  mayor = max(tam)
+  if n >= nmax
+    n
+    paso
+    break;
+  endif
+endfor
+   
