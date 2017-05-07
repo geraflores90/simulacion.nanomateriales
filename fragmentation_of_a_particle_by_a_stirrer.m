5e8ba2a7162c982d2b2d9e71e8cc0f871dcc6b6f
geraflores90 <geraflores90@gmail.com>
2017-05-06 22:39:08 -0500

    Add files via upload

--- fragmentation_of_a_particle_by_a_stirrer.m
+++ fragmentation_of_a_particle_by_a_stirrer.m
@@ -2,9 +2,9 @@ clear,clc
 
 l = 4;
 umbral = 0.1;
-n = 2; % Numero de particulas.
+n = 10; % Numero de particulas.
 d = 2; % Numero de dimensiones.
-t = 15; % Numero de pasos.
+t = 50; % Numero de pasos.
 vel = 0.5; % Valor maximo de paso dimensional permitido. STIRRER
 vel_p = zeros(1, n);
 pos = unifrnd(0.2 * l, 0.8 * l, n, d); % 0.2 y 0.8 son valores arbitrarios para que las particulas esten dentro del molino. 
@@ -12,6 +12,7 @@ tam = 20 * ones(1, n); % Vector de tamaño para n particulas.
 nmax = 100; % Maximo numero de particulas que pueden existir.
 vel_stirr = 0.01*pi;
 theta = zeros(1, n);
+st_spacing = 20;
 
 r_mill = 2;
 r_st = 1.5;
@@ -19,30 +20,33 @@ x = 0: 0.01: 2*r_mill;
 yup = sqrt(r_mill^2 - (x - r_mill).^2) + r_mill;
 ydown = -1 * sqrt(r_mill^2 - (x - r_mill).^2) + r_mill;
 mill = vertcat(horzcat(x',yup'),horzcat(x',ydown'));
-plot(mill(:, 1), mill(:, 2));
-hold on
+%plot(mill(:, 1), mill(:, 2));
+%hold on
 
-plot(pos(:, 1), pos(:, 2), 'o', 'markersize', 6);
+%plot(pos(:, 1), pos(:, 2), 'o', 'markersize', 6);
 %hold on
 
 t1 = 0.25*pi;
 t2 = 0.75*pi;
-for i = 1:50
+for i = 1:t
+  plot(mill(:, 1), mill(:, 2));
+  hold on
   for w= 1:n
     deltax = vel_p(w) * cosd(theta(w));
     deltay = vel_p(w) * sind(theta(w));
     delta = horzcat(deltax, deltay);
     pos(w,:) += delta;
   endfor
-  theta_s1 = (linspace(t1, t1, 20))';
-  theta_s2 = (linspace(t2, t2, 20))';
-  r_stirr = (linspace(-r_st, r_st, 20))';
+  theta_s1 = (linspace(t1, t1, st_spacing))';
+  theta_s2 = (linspace(t2, t2, st_spacing))';
+  r_stirr = (linspace(-r_st, r_st, st_spacing))';
   x_s1 = (r_stirr * cos(t1)) + r_mill;
   y_s1 = (r_stirr * sin(t1)) + r_mill;
   x_s2 = (r_stirr * cos(t2)) + r_mill;
   y_s2 = (r_stirr * sin(t2)) + r_mill;
   stirrer = vertcat(horzcat(x_s1,y_s1),horzcat(x_s2,y_s2));
   plot(stirrer(:, 1), stirrer(:, 2), 'o', 'markersize', 1);
+  hold on 
   t1 += vel_stirr;
   t2 += vel_stirr;
   sz = size(stirrer);
@@ -89,6 +93,7 @@ for i = 1:50
     endfor 
   endfor
   n = sig - 1;
-  plot(pos(:, 1), pos(:, 2), 'x', 'markersize', 6);
+  plot(pos(:, 1), pos(:, 2), 'o', 'markersize', 5);
   pause(0.05)
+  hold off
 endfor
